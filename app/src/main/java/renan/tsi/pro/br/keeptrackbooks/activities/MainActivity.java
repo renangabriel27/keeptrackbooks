b258998442f0330dac0a6904b3414022080c036f
renangabriel27 <renanthompsom@gmail.com>
2017-11-20 20:16:40 -0200

    Add option menu

--- app/src/main/java/renan/tsi/pro/br/keeptrackbooks/activities/MainActivity.java
+++ app/src/main/java/renan/tsi/pro/br/keeptrackbooks/activities/MainActivity.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import renan.tsi.pro.br.keeptrackbooks.R;
 
@@ -16,7 +21,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        changeScreens();
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_books:
+                changeActivity(getBaseContext(), BooksActivity.class);
+                break;
+            case R.id.action_categories:
+                changeActivity(getBaseContext(), CategoriesActivity.class);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     protected void changeToMain() {
@@ -34,45 +63,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(context, nameClass);
         startActivity(intent);
     }
-
-    private void changeScreens() {
-        changeToBooks();
-        changeToCategories();
-        changeToStatus();
-    }
-
-    private void changeToBooks() {
-        Button booksBtn = (Button) findViewById(R.id.booksBtn);
-
-        booksBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeActivity(getBaseContext(), BooksActivity.class);
-            }
-        });
-    }
-
-    private void changeToCategories() {
-        Button categoriesBtn = (Button) findViewById(R.id.categoriesBtn);
-
-        categoriesBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeActivity(getBaseContext(), CategoriesActivity.class);
-            }
-        });
-    }
-
-    private void changeToStatus() {
-        Button statusBtn = (Button) findViewById(R.id.statusBtn);
-
-        statusBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeActivity(getBaseContext(), StatusActivity.class);
-            }
-        });
-    }
-
 
 }
